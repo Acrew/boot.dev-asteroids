9db0eb22272b18d7626080a344a50642363f4b87
Pierre Wellecan <contact@mangarmor.com>
2025-06-30 10:59:58 +0200

    add Player

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import pygame
 from constants import *
 
+from player import *
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player_x = SCREEN_WIDTH / 2
+    player_y = SCREEN_HEIGHT / 2
+    player = Player(player_x, player_y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        pygame.Surface.fill(screen,'black')
+        pygame.Surface.fill(screen, 'black')
+        player.draw(screen)
+
         pygame.display.flip()
+        time = clock.tick(FPS)
+        dt = time / 1000
     #print("Starting Asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
     #print(f"Screen height: {SCREEN_HEIGHT}")
